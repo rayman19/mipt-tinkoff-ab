649f724075399a88b32a850d48011fcb44bbd2e4
rayman19 <2000egores@mail.ru>
2024-01-20 16:38:19 +0300

    bugfix App

--- src/main/scala/app/App.scala
+++ src/main/scala/app/App.scala
@@ -1,12 +1,18 @@
 package app
 
 import app.config.JsonPaths.{basePath, usernamePath, usersPath}
-import app.models.Session
+import app.models.{Session, UserJsonUtil}
 import app.models.UserJsonUtil.createEmptyJson
 import app.operations.{AccountService, Authorization}
 
+import java.io.File
+
 object App {
   def main(args: Array[String]): Unit = {
+    val file = new File(basePath)
+    if (!file.exists()) {
+      file.mkdir()
+    }
     createEmptyJson(usersPath)
     println("Авторизация")
     val session: Option[Session] = Authorization.authorizeUser
